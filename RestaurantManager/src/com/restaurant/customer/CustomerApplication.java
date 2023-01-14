725100740531175f6b4d3a264d2c827f9758c9f5
Robin <robin.reggiani@gmail.com>
2023-01-14 22:46:22 +0100

    v.0.1.03 _ Compiled gradle

---
+++ RestaurantManager/src/com/restaurant/customer/CustomerApplication.java
@@ -0,0 +1,10 @@
+package com.restaurant.customer;
+
+public class CustomerApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ RestaurantManager/src/com/restaurant/customer/GUIElements/CustomerGUI.java
@@ -0,0 +1,5 @@
+package com.restaurant.customer.GUIElements;
+
+public class CustomerGUI {
+
+}
